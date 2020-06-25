8bf12120ab52af9464429db2096c8e41f21570e3
Emily Louden <emilylouden@techforgoodinc.org>
2020-06-25 00:59:26 -0700

    server updates

---
+++ interface.py
@@ -0,0 +1,61 @@
+import psycopg2
+from psycopg2 import OperationalError
+
+### SQL INIT ###
+def create_connection():
+    connection = None
+    try:
+        connection = psycopg2.connect(database="postgres", user="postgres",
+                                      password="password", host="127.0.0.1", 
+                                      port="5432")
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+### SQL QUERY ###
+def execute_query(query, arguments=None):
+    connection = create_connection()
+    cursor = connection.cursor()
+    try:
+        if arguments is None:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, arguments)
+        connection.commit()
+        print("Query executed successfully")
+        return True
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        return False
+
+### GET TABLE VALUES ###
+def execute_read_query(query, arguments=None):
+    connection = create_connection()
+    cursor = connection.cursor()
+    result = None
+    try:
+        if arguments is None:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, arguments)
+        result = cursor.fetchall()
+        return result
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        return False
+
+
+# for debugging: export FLASK_ENV=development
+
+# in flask_app directory:
+# python3 -m venv venv
+# source venv/bin/activate
+
+# pip3 install flask
+# pip3 install requests
+# pip3 install psycopg2-binary
+
+# to run:
+# export FLASK_APP=server.py
+# flask run
--- tablecreation.py
+++ tablecreation.py
@@ -1,52 +1,29 @@
-import psycopg2
-from psycopg2 import OperationalError
+import interface as inter
 
-
-def create_connection():
-    connection = None
-    try:
-        connection = psycopg2.connect(database="postgres", user="postgres",
-                        password="password", host="127.0.0.1", port="5432")
-        print("Connection to PostgreSQL DB successful")
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-    return connection
-
-
-def execute_query(connection, query):
-    connection.autocommit = True
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Query executed successfully")
-        return True
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-        return False
-
-create_users_table = """ 
+create_users_table = """
 CREATE TABLE IF NOT EXISTS users (
   username TEXT NOT NULL,
   id INTEGER,
-  firstname TEXT NOT NULL, 
+  firstname TEXT NOT NULL,
   lastname TEXT NOT NULL,
-  address VARCHAR,
-  password VARCHAR,
-  previous_labels INTEGER
+  email TEXT NOT NULL,
+  addr VARCHAR,
+  pswd VARCHAR
 )
 """
 
 create_label_table = """
 CREATE TABLE IF NOT EXISTS labels (
-    user_id INTEGER,
+    userid INTEGER,
     destination_address VARCHAR,
     sender_address VARCHAR,
-    data DATE,
+    send_date DATE,
     price FLOAT,
     carrier VARCHAR
 )
 """
 
-con = create_connection()
-print(execute_query(con, create_users_table))
-print(execute_query(con, create_label_table))
+#print(inter.execute_query("DROP TABLE users"))
+print(inter.execute_query(create_users_table))
+print(inter.execute_query(create_label_table))
+
