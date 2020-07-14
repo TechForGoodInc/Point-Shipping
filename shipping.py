9a26cc4212e08e99b6b0dad1744963fcc8fa565b
Emily Louden <emilylouden@techforgoodinc.org>
2020-07-13 21:25:56 -0700

    easyship implementation begun

--- shipping.py
+++ shipping.py
@@ -1,81 +1,43 @@
-import easypost
-from easypost import Address
+import requests
+import json
+from urllib.request import Request, urlopen
+import json
 
-easypost.api_key = "EZAK6acab147147b466d9f28b4b65e1b8191pyE27NFXIF2v3gafaDAlcA"
-
-
-def get_address(i_name, i_street, i_city, i_state, i_zip, i_country,
-                i_company=''):
-    # add: check if address is valid
-    addr = easypost.Address.create(street1=i_street, name=i_name,
-                                   city=i_city, state=i_state, zip=i_zip,
-                                   country=i_country, verify=["delivery"])
-    if not addr.verifications["delivery"]["success"]:
-        return False
-    return addr
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer sand_uQXMIPKfuVmebaIyOFTkEO6ziXpC0W3Gswd/MUkV0Xo='
+}
 
 
-def create_parcel(length, width, height, weight):
-    parcel = easypost.Parcel.create(length=length, width=width,
-                                    height=height, weight=weight)
-    return parcel
+def create_shipment(userid, dest_name, dest_add1, dest_add2, dest_city,
+                    dest_state, dest_zip, dest_country, dest_phone,
+                    item_description, weight, height, width, length,
+                    category, currency, customs_val):
+    vals = (
+        f"\u007b\"dest_name\": \"{dest_name}\", "
+        f"\"destination_address_line_1\": \"{dest_add1}\","
+        f"\"destination_address_line_2\": \"{dest_add2}\", "
+        f"\"destination_city\": \"{dest_city}\", "
+        f"\"destination_state\": \"{dest_state}\", "
+        f"\"destination_postal_code\": {dest_zip}, "
+        f"\"destination_country_alpha2\": \"{dest_country}\", "
+        f"\"destination_phone_number\": \"{dest_phone}\", "
+        f"\"items\": \u007b\"description\": \"{item_description}\", "
+        f"\"sku\": \"test\", \"actual_weight\": {weight}, "
+        f"\"height\": {height}, \"width\": {width}, "
+        f"\"length\": {length}, \"category\": \"{category}\", "
+        f"\"declared_currency\": \"{currency}\", "
+        f"\"declared_customs_value\": {customs_val}\u007d\u007d"
+    )
+    print(type(vals))
+    print("\n")
+    print(vals)
+    print("\n")
+    package_vals = json.loads(vals)
+    print("here2")
+    request = Request('https://api.easyship.com/shipment/v1/shipments',
+                      data=package_vals, headers=headers)
 
-
-def create_flat_rate_parcel(predef, weight):
-    parcel = easypost.Parcel.create(predefined_package=predef,
-                                    weight=weight)
-    return parcel
-
-
-def create_shipment(parcel, to_addr, sender_addr):
-    shipment = easypost.Shipment.create(parcelObj=parcel,
-                                        to_address=to_addr,
-                                        from_address=sender_addr)
-    return shipment
-
-
-def get_rates(shipment):
-    to_ret = []
-    print(shipment.rates)
-    for rate in shipment.rates:
-        to_ret.append(rate)
-    return to_ret
-
-
-def shippingRates(to_address,fromAddress,parcel):
-    shipment = easypost.Shipment.create(
-        to_address=to_address,
-        from_address=fromAddress,
-        parcel=parcel)
-
-    for rate in shipment.rates:
-        print(rate.carrier)
-        print(rate.service)
-        print(rate.rate)
-        print(rate.id)
-    return rate
-
-
-# list all availabe carrier/ and user selects one favorable carrier.
-
-def selectionOfCarrier():
-    
-    #will return value of readable, which is the name of the carrier
-    
-    carriers = easypost.CarrierAccount.get('readable')
-
-    # print list of carriers' names
-
-    print(carriers)
-    
-  #allows a user to select a carrier
-
-    
-    while True:
-        readable=input()
-        if readable in easypost.CarrierAccount.keys():
-            print("You selected: "+ easypost.CarrierAccount.get(readable))
-        else:
-            print('invalid choice')
-    
-    
+    response_body = urlopen(request).read()
+    print(response_body)
+    return 0
