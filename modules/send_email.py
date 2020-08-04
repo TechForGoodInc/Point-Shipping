e950b3f55dba111e3554f20284c615171f68ce21
Emily Louden <emilylouden@techforgoodinc.org>
2020-08-04 08:45:39 -0700

    email fixes

--- modules/send_email.py
+++ modules/send_email.py
@@ -1,7 +1,7 @@
 import smtplib
 import ssl
 import random
-from modules import interface as inter
+import interface as inter
 
 
 def send_code(user_email, userid):
@@ -35,22 +35,25 @@ def send_code(user_email, userid):
         return False
 
 
-def send_url(email, url):
+def send_url(receiver_email, url):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
     sender_email = "pointshippingtest@gmail.com"
-    receiver_email = user_email
+    # disabled some security settings in gmail settings
+    password = "Pointshipping12"
     message = f"""\
-	Subject: Your Point Shipping Label
-	Please print out this label and affix it to your package.
-	{url}"""
-    print(message)
+        Subject: Your Point Shipping Label
+
+        Please print out this label and affix it to your package.
+        {url}"""
     try:
         context = ssl.create_default_context()
-        print("here")
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message)
-        return True
-    except smtplib.SMTPRecipientsRefused:
+            return True
+    except smtplib.SMTPException:
         return False
+
+
+print(send_url("pointshippingtest@gmail.com", "hi"))
