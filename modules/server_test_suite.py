288c404d093170a6bfe2407746ec977f61a8721c
Emily Louden <emilylouden@techforgoodinc.org>
2020-08-22 12:20:57 -0700

    server update

--- modules/server_test_suite.py
+++ modules/server_test_suite.py
@@ -132,9 +132,3 @@ create_payment_method = {'default': 'False', 'card_num': '4242424242424242',
 
 
 charge_card = {'payment_token': 'pm_1H9yYXAzJnRyZcvUNdL1Sa6H', 'amount': 123.4}
-
-
-resp = requests.post('http://127.0.0.1:5000/getrates/', data=rate_input_true)
-print(resp.content)
-resp = requests.post('http://127.0.0.1:5000/addpackage/', data=shipping_input_true)
-print(resp.content)
